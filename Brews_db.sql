9e8aa7fefd9df9baf9fd3b712795733b65e2239c
summers96 <darrius.summers90@gmail.com>
2021-03-22 15:29:55 -0400

    ETL

---
+++ Brews_db.sql
@@ -0,0 +1,41 @@
+DROP TABLE bars_nc
+
+DROP TABLE bars_sc
+
+DROP TABLE breweries_data
+
+DROP TABLE breweries_combined
+
+ALTER TABLE breweries_data
+ADD COLUMN price text,
+ADD COLUMN rating double precision,
+ADD COLUMN review_count double precision;
+
+SELECT * FROM bars_nc
+
+SELECT * FROM bars_sc
+
+SELECT * FROM breweries_data
+
+SELECT * FROM breweries_combined
+
+SELECT * FROM breweries
+
+-- Merge tables
+CREATE TABLE breweries_combined 
+ AS
+ SELECT * FROM breweries_data 
+ 	UNION
+  	SELECT * FROM bars_sc
+
+CREATE TABLE breweries
+ AS
+ SELECT * FROM breweries_combined
+ 	UNION
+  	SELECT * FROM bars_nc
+	
+	
+SELECT * 
+FROM breweries
+WHERE 
+	city = 'Durham';
