80daf7dfaf06645350befe68f48f4f755cad0f5f
Melinda <melindaeudy@gmail.com>
2021-03-22 10:36:56 -0400

    finished all notations in jupyter notebook and PgAdmin tables

--- Brews_db.sql
+++ Brews_db.sql
@@ -5,9 +5,9 @@ DROP TABLE bars_sc
 DROP TABLE breweries_data
 
 ALTER TABLE breweries_data
-ADD COLUMN price VARCHAR,
-ADD COLUMN rating VARCHAR,
-ADD COLUMN review_count VARCHAR;
+ADD COLUMN price text,
+ADD COLUMN rating double precision,
+ADD COLUMN review_count double precision;
 
 SELECT * FROM bars_nc
 
@@ -15,17 +15,19 @@ SELECT * FROM bars_sc
 
 SELECT * FROM breweries_data
 
+SELECT * FROM breweries_combined
 
+SELECT * FROM breweries
 
 -- Merge tables
-INSERT INTO breweries_data 
-(SELECT FROM bars_sc WHERE address1 NOT IN (SELECT FROM breweries_data));
-
 CREATE TABLE breweries_combined 
  AS
  SELECT * FROM breweries_data 
  	UNION
   	SELECT * FROM bars_sc
 
-To merge rows from one table into the other. INSERT INTO table1 (SELECT * FROM table2 WHERE name NOT IN (SELECT name FROM table1));
-For creating new table from old tables. CREATE TABLE new_table AS (SELECT * FROM table1 UNION SELECT * FROM table2);
+CREATE TABLE breweries
+ AS
+ SELECT * FROM breweries_combined 
+ 	UNION
+  	SELECT * FROM bars_nc
